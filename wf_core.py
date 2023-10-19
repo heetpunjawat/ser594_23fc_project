d3df3458cce737260a2e57b170addada88493fe1
Heet Punjawat <hpunjawa@asu.edu>
2023-10-18 18:42:02 -0700

    Adding code for wf_core.py

---
+++ wf_core.py
@@ -0,0 +1,16 @@
+import subprocess
+
+def execute_files():
+    # List of Python files to be executed in order
+    file_paths = ['wf_dataprocessing.py', 'wf_visualization.py']
+
+    for file_path in file_paths:
+        try:
+            # Run the Python file using subprocess
+            subprocess.run(['python', file_path], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing {file_path}: {e}")
+            break
+
+if __name__ == "__main__":
+    execute_files()
